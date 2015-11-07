03f6b6c75199744499d6a664e7fc057b0793336d
Felix Hartmann <ffhmichels@gmail.com>
2015-11-07 11:32:51 +0100

    Aufräumen

--- app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/ItemActivty.java
+++ app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/ItemActivty.java
@@ -1,4 +1,4 @@
-package refuhack.bitspls.de.hstuttgart15.activities.activities;
+package refuhack.bitspls.de.hstuttgart15.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import refuhack.bitspls.de.hstuttgart15.R;
-import refuhack.bitspls.de.hstuttgart15.activities.MainActivity;
 
 public class ItemActivty extends AppCompatActivity {
     @Override
