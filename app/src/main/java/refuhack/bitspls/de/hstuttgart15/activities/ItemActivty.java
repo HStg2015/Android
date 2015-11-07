39efbfce12bc35741a11b343474718483f0fe388
Felix Hartmann <ffhmichels@gmail.com>
2015-11-07 01:57:25 +0100

    ok

--- app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/ItemActivty.java
+++ app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/ItemActivty.java
@@ -1,33 +1,40 @@
 package refuhack.bitspls.de.hstuttgart15.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ScrollView;
 
 import refuhack.bitspls.de.hstuttgart15.R;
 
 public class ItemActivty extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_activty);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                callButtonPressed();
             }
         });
+
     }
+
+    private void callButtonPressed() {
+        //Todo
+    }
+
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(ItemActivty.this, MainActivity.class);
