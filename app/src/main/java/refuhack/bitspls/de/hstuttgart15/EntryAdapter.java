ce3f98027534747ec13814f0622fafa5e2670e4e
Gin <felix.burk@googlemail.com>
2015-11-07 12:21:39 +0100

    added dynamical list for our entrys serveral bugs like in here! TODO

---
+++ app/src/main/java/refuhack/bitspls/de/hstuttgart15/EntryAdapter.java
@@ -0,0 +1,61 @@
+package refuhack.bitspls.de.hstuttgart15;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by gin on 07.11.15.
+ */
+public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHolder> {
+
+    private List<Entry> entryList;
+
+    public EntryAdapter(List<Entry> entryList) {
+        this.entryList = entryList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return entryList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(EntryViewHolder entryViewHolder, int i) {
+        Entry entry = entryList.get(i);
+        entryViewHolder.vName.setText(entry.name);
+        entryViewHolder.vDescription.setText(entry.description);
+        entryViewHolder.vImage.setImageResource(entry.photoId);
+    }
+
+    @Override
+    public EntryViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.content_main, viewGroup, false);
+
+        return new EntryViewHolder(itemView);
+    }
+
+    public class EntryViewHolder extends RecyclerView.ViewHolder {
+        protected TextView vName;
+        protected TextView vDescription;
+        protected ImageView vImage;
+
+        public EntryViewHolder(View v){
+            super(v);
+            vName = (TextView) v.findViewById(R.id.person_name);
+            vDescription = (TextView) v.findViewById(R.id.person_age);
+            vImage = (ImageView) v.findViewById(R.id.person_photo);
+        }
+    }
+
+
+}
+
+
--- app/src/main/java/refuhack/bitspls/de/hstuttgart15/MainActivity.java
+++ app/src/main/java/refuhack/bitspls/de/hstuttgart15/MainActivity.java
@@ -3,6 +3,9 @@ package refuhack.bitspls.de.hstuttgart15;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,8 +18,14 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private List<Entry> entryList;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +52,27 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ImageView image = (ImageView) findViewById(R.id.person_photo);
-        image.setImageResource(R.drawable.max);
+        rv = (RecyclerView)findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
 
-        TextView text = (TextView) findViewById(R.id.person_name);
-        text.setText("FelixB");
+        GridLayoutManager glm = new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false);
+        rv.setLayoutManager(glm);
 
+        initializeData();
+        initializeAdapter();
+
+    }
+
+    private void initializeData(){
+        entryList = new ArrayList<>();
+        entryList.add(new Entry("Felix B", "Android Dev", R.drawable.max));
+        entryList.add(new Entry("Max Mustermann", "User", R.drawable.max));
+        entryList.add(new Entry("Max", "Guy", R.drawable.max));
+    }
+
+    private void initializeAdapter(){
+        EntryAdapter adapter = new EntryAdapter(entryList);
+        rv.setAdapter(adapter);
     }
 
     @Override
