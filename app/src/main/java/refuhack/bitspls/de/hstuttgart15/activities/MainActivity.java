0463f5a4487f2458d788abaea7673594da8a5446
Lasse Riess <lasse.riess@gmail.com>
2015-11-08 08:52:18 +0100

    printing

--- app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/MainActivity.java
+++ app/src/main/java/refuhack/bitspls/de/hstuttgart15/activities/MainActivity.java
@@ -1,9 +1,22 @@
 package refuhack.bitspls.de.hstuttgart15.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.print.PrintHelper;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,9 +31,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.joda.time.DateTime;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -50,9 +66,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), EintragHinzufuegenFragment.class);
-                startActivity(intent);
-
+                //Intent intent = new Intent(view.getContext(), EintragHinzufuegenFragment.class);
+                //startActivity(intent);
+                //doPhotoPrint();
+                doWebViewPrint();
             }
         });
 
@@ -134,5 +151,54 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private WebView mWebView;
+
+    private void doWebViewPrint() {
+        // Create a WebView object specifically for printing
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i("MainActivity", "page finished loading " + url);
+                createWebPrintJob(view);
+                mWebView = null;
+            }
+        });
+
+        // Generate an HTML document on the fly:
+        String html = "<html><body>";
+        ArrayList<Entry> entries = EntryStorage.getInstance().getList();
+        for(int i = 0; i< 2; i++){
+            Entry curr = entries.get(i);
+            html = html+"<h3>"+curr.getName()+"</h3> <br/> <h4>" + curr.getDescription()+
+                    "</h4><br/><br/><p>Stadtteil:"+curr.getZipcode()+"</p><br/><br/><br/><br/><br/>";
+        }
+        html = html + "</body></html>";
+        webView.loadDataWithBaseURL(null, html, "text/HTML", "UTF-8", null);
+
+        // Keep a reference to WebView object until you pass the PrintDocumentAdapter
+        // to the PrintManager
+        mWebView = webView;
+    }
+
+    private void createWebPrintJob(WebView webView) {
+
+        // Get a PrintManager instance
+        PrintManager printManager = (PrintManager) getSystemService(Context.PRINT_SERVICE);
+
+        // Get a print adapter instance
+        PrintDocumentAdapter printAdapter = webView.createPrintDocumentAdapter();
+
+        // Create a print job with name and adapter instance
+        String jobName = getString(R.string.app_name) + " Document";
+        PrintJob printJob = printManager.print(jobName, printAdapter,
+                new PrintAttributes.Builder().build());
     }
 }
